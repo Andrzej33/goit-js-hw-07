ef6a6d75520b4f8cc13917d2b381c6089e4e8dfa
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-12 19:56:55 +0200

    hw done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,7 +11,7 @@ const fillcreateGalery = createGalery(galleryItems);
 
 // додаємо галерею зображень в HTML
 
-galeryContainer.insertAdjacentHTML('beforeend', fillcreateGalery);
+galeryContainer.insertAdjacentHTML("beforeend", fillcreateGalery);
 
 // прописуємо функцію для створення галереї
 
@@ -32,71 +32,31 @@ function createGalery(galleryItems) {
     .join("");
 }
 
-
 // Додаємо слухача подій на клік по контейнеру
 
-galeryContainer.addEventListener('click', onGaleryItemClick);
+galeryContainer.addEventListener("click", onGaleryItemClick);
 
 // створюємо функцію для обробки кліку по зображенню
 
-function onGaleryItemClick(event) { 
+function onGaleryItemClick(event) {
   event.preventDefault();
+
+  // перевіряємо чи клік відбувся на зображенні
+
   if (!event.target.classList.contains(`gallery__image`)) {
     return;
   }
-    
-    const instance = basicLightbox.create(`
-    <img src= ${event.target.dataset.source}>
- `,)
-instance.show()
+  // створюємо велике зображення за допомогою бібліотеки
 
-  document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape' && instance.visible()) {
-    instance.close()
-   
-  }
-})
-  
-};
+  const instance = basicLightbox.create(`
+    <img src= ${event.target.dataset.source}>`);
+  instance.show();
 
+  // додаємо закривання модалки по кліку на Esc
 
-
-
-// Файл gallery-items.js містить масив galleryItems, який містить об'єкти з інформацією про зображення: маленьке (прев`ю), оригінальне (велике) і опис. Ми вже підключили його до кожного з JS-файлів проекту.
-
-// Завдання 1 - галерея зображень
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
-
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>
-
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
-
-// Закриття з клавіатури
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-
-// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
-
-
-
-
-
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && instance.visible()) {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galeryContainer = document.querySelector(".gallery");
@@ -9,33 +9,26 @@ const fillcreateGalery = createGalery(galleryItems);
 
 // додаємо галерею зображень в HTML
 
-galeryContainer.insertAdjacentHTML('beforeend', fillcreateGalery);
+galeryContainer.insertAdjacentHTML("beforeend", fillcreateGalery);
 
 // прописуємо функцію для створення галереї
 
 function createGalery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
+      return `<li class="gallery__card">
   <a class="gallery__item" href="${original}">
      <img
       class="gallery__image"
       src="${preview}"
-       data-source= "${original}"
-       alt="${description}"
-            />
-   </a>
- </li>`;
+       alt="${description}"/></a></li>`;
     })
     .join("");
 }
 
-new SimpleLightbox('.gallery a', {
-    captionsData: "alt",captionDelay: 250});
+// створюємо модалку з бібліотеки
 
-
-// {/* <a class="gallery__item" href="large-image.jpg">
-//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
-// </a> */}
-
-console.log(galleryItems);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
